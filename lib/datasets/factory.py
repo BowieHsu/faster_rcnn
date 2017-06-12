a65f0862e224c79adc33ebb5bab17c656b119528
BowenXu <hjc336@163.com>
2017-06-12 14:43:10 +0800

    Add files via upload

---
+++ lib/datasets/factory.py
@@ -0,0 +1,50 @@
+# --------------------------------------------------------
+# Fast R-CNN
+# Copyright (c) 2015 Microsoft
+# Licensed under The MIT License [see LICENSE for details]
+# Written by Ross Girshick
+# --------------------------------------------------------
+
+"""Factory method for easily getting imdbs by name."""
+
+__sets = {}
+
+from datasets.pascal_voc import pascal_voc
+from datasets.coco import coco
+from datasets.railway import railway
+# from datasets.insulator import insulator
+import numpy as np
+
+# set up railway datasets
+for year in ['20170611']:
+    for split in ('trainval', 'test'):
+        name = 'railway_{}_{}'.format(year, split)
+        __sets[name] = (lambda  split=split, year=year:railway(split, year))
+
+# Set up voc_<year>_<split> using selective search "fast" mode
+for year in ['2007', '2012']:
+    for split in ['train', 'val', 'trainval', 'test']:
+        name = 'voc_{}_{}'.format(year, split)
+        __sets[name] = (lambda split=split, year=year: pascal_voc(split, year))
+
+# Set up coco_2014_<split>
+for year in ['2014']:
+    for split in ['train', 'val', 'minival', 'valminusminival']:
+        name = 'coco_{}_{}'.format(year, split)
+        __sets[name] = (lambda split=split, year=year: coco(split, year))
+
+# Set up coco_2015_<split>
+for year in ['2015']:
+    for split in ['test', 'test-dev']:
+        name = 'coco_{}_{}'.format(year, split)
+        __sets[name] = (lambda split=split, year=year: coco(split, year))
+
+def get_imdb(name):
+    """Get an imdb (image database) by name."""
+    if not __sets.has_key(name):
+        raise KeyError('Unknown dataset: {}'.format(name))
+    return __sets[name]()
+
+def list_imdbs():
+    """List all registered imdbs."""
+    return __sets.keys()
